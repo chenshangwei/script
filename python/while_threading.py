446d4b8ad73cc1a0ba54c3a8f463467d88d96688
陈尚伟 <chenshangwei@users.noreply.github.com>
2014-12-12 22:51:14 +0800

    Create while_threading.py

---
+++ python/while_threading.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# while_threading.py
+# 秒级计划任务 多线程实现
+# script ：Second plan task,by threading
+import urllib2,time,sys,os
+import threading
+import signal
+
+help = '''usage : nohup ./while_theading.py &
+          test  : python while_threading.py
+@notice Before use，please configure the "config",and make sure that urls are ok!
+@author Cookie Chen
+@update 2014-12-12
+For more information : 
+'''
+config = [
+        {'url':'http://xxoo.com/test/test1','sleep':1},
+        {'url':'http://xxoo.com/test/test2','sleep':2},
+        {'url':'http://xxoo.com/test/test3','sleep':3},
+]
+
+def getUrl(url,sleep):
+    print '[Mission start] ','-',url,'-',sleep
+    while True:
+        print time.ctime(),'[Runing] ',url,' ',sleep
+        urllib2.urlopen(url)
+        if sleep > 0:
+            time.sleep(sleep)
+
+if __name__ == '__main__':
+    t = []
+    event = threading.Event()
+    for val in config:
+        t.append(threading.Thread(target=getUrl,args=(val['url'],val['sleep'])))
+
+    for p in t:
+        p.setDaemon(True)
+        p.start()
+
+    signal.signal(signal.SIGINT,lambda *args: event.set())
+    signal.signal(signal.SIGTERM,lambda *args: event.set())
+
+    while not event.isSet():
+        time.sleep(60)
+        
+    print 'Task quit!'
+    sys.exit(0)
