39763ec411c5d4677337ed6a2692afb8fe02e29b
cookieChen <chenshangwei@users.noreply.github.com>
2015-06-26 17:12:15 +0800

    Update backup_db.py

--- python/backup_db.py
+++ python/backup_db.py
@@ -4,8 +4,8 @@
 Backup for mysql
 Two way:
   A mysqldump to .sql file
-  B copy all data file
-  All backup fileS will be send to ftp-server
+  B send all data file
+  All backup files will be send to ftp-server
 Usage : python backup_db.py
 '''
 __author__ = 'cookieChen https://github.com/chenshangwei'
@@ -15,13 +15,19 @@ import os,sys
 import logging,time
 
 ''' config '''
+BACKUP_TYPE = 'A' # A OR B
+IS_TAR_GZ = False  # False OR True
+
 FTP_HOST = ''
 FTP_NAME = ''
 FTP_PWD = ''
 
 MYSQL_PORT = 3306
-MYSQL_PREFIX = '/usr/local/mysql/'
+MYSQL_PREFIX = '/usr/local/mysql/' 
+MYSQL_DATA_DIR = '/data/mysql1'                                                                                                          
 
-DATA_DIR='/data/mysql1'                                                                                                                   
 TMP_DIR = '/data/backup/'
 EXCLUDE_DIR = ['mysql','test','log']
+INNODB_FILE = ['ibdata1','ib_logfile0','ib_logfile1']
+
+
