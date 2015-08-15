fc37967b58b81cb38564490e5001565e7470aca8
cookieChen <chenshangwei@users.noreply.github.com>
2015-08-15 16:59:21 +0800

    Update backup_conf.py

--- python/backup_conf.py
+++ python/backup_conf.py
@@ -1,4 +1,18 @@
 
 '''
-备份各种配置文件
+备份各种文件
 '''
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+    
+print get_ip_address('lo')
+print get_ip_address('em1')
